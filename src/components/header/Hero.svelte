60cd54c4372e8c718e0cab963aec7506a3be7d46
vikbert <segentor@gmail.com>
2021-03-08 08:03:49 +0100

    updates

--- src/components/header/Hero.svelte
+++ src/components/header/Hero.svelte
@@ -1,15 +1,32 @@
-<div class="hero is-secondary">
+<div class="hero is-secondary hide-mobile">
   <div class="container">
+    <div class="row">
+      <div class="column description">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/1/1c/Dict.cc_Logo.svg"
           alt="dict.cc"
         />
         <h1>toll</h1>
-        <h6>Englisch: awesome [coll.][excellent]</h6>
+        <p class='opacity-50'>Englisch: awesome [coll.][excellent]</p>
+        <p>all resources are from <strong>GitHub Awesome</strong> Projects</p>
+
+      </div>
+      <div class="column opacity-75 picture hide-mobile">
+        <span class="iconify" data-icon="fe:github-alt" data-inline="false" width=100></span>
+        <span class="iconify" data-icon="simple-icons:awesomelists" data-inline="false" width=100></span>
+      </div>
+    </div>
   </div>
 </div>
+
 <style>
   img {
-    width: 8rem;
+    width: 10rem;
+  }
+  .hero {
+    background-color: #1c0226;
+  }
+  .picture > .iconify {
+    margin: 10px;
   }
 </style>
